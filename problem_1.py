d3b65f28d9f082efc1181b06645698a6debb2f6c
rohitkokane247-ctrl <rohitkokane247@gmail.com>
2026-05-28 09:44:56 +0530

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -3,8 +3,11 @@
 
 # Write code here
 
-def seconds_in_week():
+#calculate seconds in a day 
+seconds_in_day = 24*60*60
 
-  return(result)
+#calculate seconds in a week
+seconds_in_week = seconds_in_day*7
 
 print(seconds_in_week)
+
